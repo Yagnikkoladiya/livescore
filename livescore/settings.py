9cd759fd11888c2000d7f8be01022a03a006da2e
Yagnikkoladiya <102154790+Yagnikkoladiya@users.noreply.github.com>
2022-04-15 12:26:37 +0530

    change

--- livescore/settings.py
+++ livescore/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!b66#p+(r8j#_r54*-u+%!!8hu8n_#&_pismpa&zj)crbyv%!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','livecscore.herokuapp.com/']
+
 
 
 # Application definition
